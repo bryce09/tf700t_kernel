a7005460955ea2d41579d7373c6fe9f6d9ab3920
Alex S <alexander.skerhut@gmail.com>
2013-04-14 21:33:54 +0200

    actmon: decrease latency might improve performance on emc

--- arch/arm/mach-tegra/tegra3_actmon.c
+++ arch/arm/mach-tegra/tegra3_actmon.c
@@ -500,13 +500,13 @@ static struct actmon_dev actmon_dev_emc = {
 	.boost_freq_step	= 16000,
 	.boost_up_coef		= 200,
 	.boost_down_coef	= 50,
-	.boost_up_threshold	= 60,
-	.boost_down_threshold	= 40,
+	.boost_up_threshold	= 30,	//60
+	.boost_down_threshold	= 20,	//40
 
 	.up_wmark_window	= 1,
 	.down_wmark_window	= 3,
 	.avg_window_log2	= ACTMON_DEFAULT_AVG_WINDOW_LOG2,
-	.count_weight		= 0x200,
+	.count_weight		= 0x100,
 
 	.type			= ACTMON_FREQ_SAMPLER,
 	.state			= ACTMON_UNINITIALIZED,
@@ -532,8 +532,8 @@ static struct actmon_dev actmon_dev_avp = {
 	.boost_freq_step	= 8000,
 	.boost_up_coef		= 200,
 	.boost_down_coef	= 50,
-	.boost_up_threshold	= 85,
-	.boost_down_threshold	= 50,
+	.boost_up_threshold	= 40,	//85
+	.boost_down_threshold	= 25,	//50
 
 	.up_wmark_window	= 1,
 	.down_wmark_window	= 3,
